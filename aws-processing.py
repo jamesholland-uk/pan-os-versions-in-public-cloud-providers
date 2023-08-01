1ff70391f8227c5c84f81be28190a46f1219eae4
lachlanjholmes <1935116+lachlanjholmes@users.noreply.github.com>
2023-08-01 19:50:47 +1000

    Adding Panorama for AWS. (#4)

--- aws-processing.py
+++ aws-processing.py
@@ -1,6 +1,8 @@
 import sys
 import boto3
 import re
+from packaging import version
+import os
 
 """
 Product Codes from Palo's Doc site:
@@ -9,6 +11,7 @@ https://docs.paloaltonetworks.com/vm-series/11-0/vm-series-deployment/set-up-the
 byol = '6njl1pau431dv1qxipg63mvah'
 bundle1 = 'e9yfvyj3uag5uo5j2hjikv74n'
 bundle2 = 'hd44w1chf26uv4p52cdynb2o'
+panorama = 'eclz7j04vu9lf8ont8ta3n17o'
 marketplace_owner_id = '679593333241'
 
 # Create a boto3 session
@@ -23,10 +26,12 @@ try:
     list_of_byol = []
     list_of_bundle1 = []
     list_of_bundle2 = []
+    list_of_panorama = []
     # Initialise an empty dictionaries to store AMI IDs based on licensing model
     amis_by_version_byol = {}
     amis_by_version_bundle1 = {}
     amis_by_version_bundle2 = {}
+    amis_by_version_panorama = {}
 
     for region in regions:
         # Create an EC2 client for the region
@@ -109,12 +114,81 @@ try:
                     # Add the AMI ID to the dictionary, with the region as the key
                     amis_by_version_bundle2[ver][region] = ami['ImageId']
 
+        ami_images = ec2.describe_images(Filters=[{'Name': 'name', 'Values': ['Panorama-AWS*']}])
+
+        # Iterate through each AMI image
+        for ami in ami_images['Images']:
+            # Check if AMI is BYOL and owned by the marketplace owner
+            if ami['ProductCodes'][0]['ProductCodeId'] == panorama and ami['OwnerId'] == marketplace_owner_id:
+                # Extract the version number from the AMI name
+                # Check if the version is a hotfix (i.e. ends with "-hXX")
+                match = re.search(r'.*Panorama-AWS-(\d+.\d+.\d-h\d{1,2})-[a-z|0-9|-]{36}', ami['Name'])
+                if match:
+                    # Extract the version number from the match
+                    ver = match.group(1)
+                    list_of_panorama.append(ver)
+                    if ver not in amis_by_version_panorama:
+                        amis_by_version_panorama[ver] = {}
+                    amis_by_version_panorama[ver][region] = ami['ImageId']
+                else:
+                    # Split the AMI name by hyphens and extract the 3th element (i.e. the version number)
+                    ver = ami['Name'].split("-")[2]
+                    # Add the version number to the list of BYOL versions
+                    list_of_panorama.append(ver)
+                    # If the version number is not already a key in the dictionary, add it
+                    if ver not in amis_by_version_panorama:
+                        amis_by_version_panorama[ver] = {}
+                    # Add the AMI ID to the dictionary, with the region as the key
+                    amis_by_version_panorama[ver][region] = ami['ImageId']                    
+
     list_of_byol = list(dict.fromkeys(list_of_byol))
-    list_of_byol.sort()
+    list_of_byol.sort(key=version.LegacyVersion)
+    # print("list_of_byol:",list_of_byol)
     list_of_bundle1 = list(dict.fromkeys(list_of_bundle1))
-    list_of_bundle1.sort()
+    list_of_bundle1.sort(key=version.LegacyVersion)
+    # print("list_of_bundle1:",list_of_bundle1)
     list_of_bundle2 = list(dict.fromkeys(list_of_bundle2))
-    list_of_bundle2.sort()
+    list_of_bundle2.sort(key=version.LegacyVersion)
+    # print("list_of_bundle2:",list_of_bundle2)
+    list_of_panorama = list(dict.fromkeys(list_of_panorama))
+    list_of_panorama.sort(key=version.LegacyVersion)
+    # print("list_of_panorama:",list_of_panorama)
+    
+    # Purge all the files under the aws/byol/ folder
+    byol_folder = 'aws/byol/'
+    for file in os.listdir(byol_folder):
+        file_path = os.path.join(byol_folder, file)
+        if os.path.isfile(file_path):
+            # print(file_path)
+            os.remove(file_path)
+
+    # Purge all the files under the aws/bundle1/ folder
+    bundle1_folder = 'aws/bundle1/'
+    for file in os.listdir(bundle1_folder):
+        file_path = os.path.join(bundle1_folder, file)
+        if os.path.isfile(file_path):
+            # print(file_path)
+            os.remove(file_path)
+
+    # Purge all the files under the aws/bundle2/ folder
+    bundle2_folder = 'aws/bundle2/'
+    for file in os.listdir(bundle2_folder):
+        file_path = os.path.join(bundle2_folder, file)
+        if os.path.isfile(file_path):
+            # print(file_path)
+            os.remove(file_path)
+
+
+    # Purge all the files under the aws/panorama/ folder
+    panorama_folder = 'aws/panorama/'
+    for file in os.listdir(panorama_folder):
+        file_path = os.path.join(panorama_folder, file)
+        if os.path.isfile(file_path):
+            # print(file_path)
+            os.remove(file_path)
+
+
+    # Create the aws.md file
     result = ""
     result += "\n# AWS\n"
     result += "\n### BYOL\n"
@@ -126,6 +200,9 @@ try:
     result += "\n### PAYG Bundle 2\n"
     for version in list_of_bundle2:
         result += "- [" + version + "](aws/bundle2/" + version + ".md) \n"
+    result += "\n### PAYG Panorama\n"
+    for version in list_of_panorama:
+        result += "- [" + version + "](aws/panorama/" + version + ".md) \n"
     result += "\n"
     with open('aws.md','w') as file:
         file.write(result)
@@ -159,6 +236,16 @@ try:
             ver_str += '- ' + region + ': ' + ami + '\n'
         ver_str += '\n[Go back to aws.md](../../aws.md) \n'
         with open('aws/bundle2/' + ver + '.md', 'w') as f:
+            f.write(ver_str)
+
+    # Add the AMI IDs for each version to the markdown string
+    for ver, amis in amis_by_version_panorama.items():
+        ver_str = ''
+        ver_str += '\n # '+ ver + '\n'
+        for region, ami in amis.items():
+            ver_str += '- ' + region + ': ' + ami + '\n'
+        ver_str += '\n[Go back to aws.md](../../aws.md) \n'
+        with open('aws/panorama/' + ver + '.md', 'w') as f:
             f.write(ver_str)
 
 except Exception as e:
